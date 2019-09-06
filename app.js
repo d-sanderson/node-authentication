cfc348413d469f1f126f461f0921c53bfff2f067
dsanderson90 <dsanderson3@cnm.edu>
2019-09-05 21:46:36 -0600

    everything is working!

--- app.js
+++ app.js
@@ -1,13 +1,56 @@
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
+const mongoose = require('mongoose');
+const flash = require('connect-flash');
+const session = require('express-session');
+const passport = require('passport')
+
+const dotenv = require('dotenv');
+dotenv.config();
 
 const app = express();
 
+//  Passport Config
+require('./config/passport')(passport);
+// DB Connection
+const db = process.env.MONGODB_URI
+mongoose.connect(db, { useNewUrlParser: true})
+.then(() => console.log('Mongo connected...'))
+.catch(err => console.log(err));
+
+
 // EJS
+app.set('view engine', 'ejs');
 app.use(expressLayouts);
 //  app.set(...) must be below app.use(expressLayout)
-app.set('view engine', 'ejs');
 
+//  Bodyparser
+app.use(express.urlencoded({ extended: false }));
+
+//  Express session
+app.use(session({
+  secret: 'secret',
+  resave: true,
+  saveUninitialized: true,
+}));
+
+//  Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+//  Connect Flash
+app.use(flash());
+
+// Global Variables
+app.use((req, res, next) => {
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  next();
+});
+
+
+//  Routes
 app.use('/', require('./routes/index'));
 app.use('/users', require('./routes/users'));
 
